1c2e32d862418b2d25413a4559883fbefaceb908
fyhao <fyhao1@gmail.com>
2022-04-05 16:02:47 +0000

    add correct e164 validation logic

--- lib/src/main/java/com/fyhao/phonenumbervalidator/PhoneNumberValidator.java
+++ lib/src/main/java/com/fyhao/phonenumbervalidator/PhoneNumberValidator.java
@@ -1,8 +1,14 @@
 package com.fyhao.phonenumbervalidator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhoneNumberValidator {
 
     public boolean isValidPhoneNumber(String number) {
-        return true;
+        String regex = "^\\+(?:[0-9] ?){6,14}[0-9]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(number);
+        return matcher.matches();
     }
 }
--- lib/src/test/java/com/fyhao/phonenumbervalidator/PhoneNumberValidatorTest.java
+++ lib/src/test/java/com/fyhao/phonenumbervalidator/PhoneNumberValidatorTest.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class PhoneNumberValidatorTest {
     @Test void testForValidation() {
         PhoneNumberValidator lib = new PhoneNumberValidator();
-        assertTrue(lib.isValidPhoneNumber("1234"));
+        assertTrue(lib.isValidPhoneNumber("+6591234123"));
     }
 }
